7bfcaa486cef2cf987f1e7aa36a30d8420cbf0d1
TanThinh <thinhhtse183224@fpr.edu.vn>
2025-09-29 12:37:46 +0700

    feat(parking-lot-service): change the path of ParkingFloorController

--- parking-lot-service/src/main/java/com/parkmate/floor/FloorController.java
+++ parking-lot-service/src/main/java/com/parkmate/floor/FloorController.java
@@ -13,7 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/parking-floors")
+@RequestMapping("/api/floors")
 @RequiredArgsConstructor
 @Tag(name = "Parking Floor API", description = "API for making and configuring Parking Floor")
 @Validated
