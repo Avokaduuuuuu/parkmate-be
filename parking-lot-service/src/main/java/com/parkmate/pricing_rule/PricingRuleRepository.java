cd073e26ca0aaa53815189127954347ae5fc5a40
TanThinh <thinhhtse183224@fpr.edu.vn>
2025-10-21 13:23:16 +0700

    feat(parking-lot-service): add API to fetch pricing rules for synchronization

--- parking-lot-service/src/main/java/com/parkmate/pricing_rule/PricingRuleRepository.java
+++ parking-lot-service/src/main/java/com/parkmate/pricing_rule/PricingRuleRepository.java
@@ -1,9 +1,11 @@
 package com.parkmate.pricing_rule;
 
+import com.parkmate.session.enums.SyncStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PricingRuleRepository extends JpaRepository<PricingRuleEntity, Long> {
-
+    List<PricingRuleEntity> findAllByParkingLotIdAndSyncStatus(Long lotId, SyncStatus syncStatus);
 }
